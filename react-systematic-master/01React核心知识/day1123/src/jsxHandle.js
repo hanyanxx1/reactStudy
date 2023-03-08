7b9fe621a83b88b5b92b9acc53d3af8e99d82839
hanyanxx1 <hanyanxx1@163.com>
2023-03-08 11:36:43 +0800

    230307 -  09.函数组件的底层渲染机制、10.关于props属性的细节知识

--- react-systematic-master/01React核心知识/day1123/src/jsxHandle.js
+++ react-systematic-master/01React核心知识/day1123/src/jsxHandle.js
@@ -10,75 +10,77 @@
       let keys = Reflect.ownKeys(arr);
 */
 const each = function each(obj, callback) {
-    if (obj === null || typeof obj !== "object") throw new TypeError('obj is not a object');
-    if (typeof callback !== "function") throw new TypeError('callback is not a function');
-    let keys = Reflect.ownKeys(obj);
-    keys.forEach(key => {
-        let value = obj[key];
-        // 每一次迭代，都把回调函数执行
-        callback(value, key);
-    });
+  if (obj === null || typeof obj !== "object")
+    throw new TypeError("obj is not a object");
+  if (typeof callback !== "function")
+    throw new TypeError("callback is not a function");
+  let keys = Reflect.ownKeys(obj);
+  keys.forEach((key) => {
+    let value = obj[key];
+    // 每一次迭代，都把回调函数执行
+    callback(value, key);
+  });
 };
 
 /* createElement:创建虚拟DOM对象 */
 export function createElement(ele, props, ...children) {
-    let virtualDOM = {
-        $$typeof: Symbol('react.element'),
-        key: null,
-        ref: null,
-        type: null,
-        props: {}
+  let virtualDOM = {
+    $$typeof: Symbol("react.element"),
+    key: null,
+    ref: null,
+    type: null,
+    props: {},
+  };
+  let len = children.length;
+  virtualDOM.type = ele;
+  if (props !== null) {
+    virtualDOM.props = {
+      ...props,
     };
-    let len = children.length;
-    virtualDOM.type = ele;
-    if (props !== null) {
-        virtualDOM.props = {
-            ...props
-        };
-    }
-    if (len === 1) virtualDOM.props.children = children[0];
-    if (len > 1) virtualDOM.props.children = children;
-    return virtualDOM;
-};
+  }
+  if (len === 1) virtualDOM.props.children = children[0];
+  if (len > 1) virtualDOM.props.children = children;
+  return virtualDOM;
+}
 
 /* render:把虚拟DOM变为真实DOM */
 export function render(virtualDOM, container) {
-    let { type, props } = virtualDOM;
-    if (typeof type === "string") {
-        // 存储的是标签名:动态创建这样一个标签
-        let ele = document.createElement(type);
-        // 为标签设置相关的属性 & 子节点
-        each(props, (value, key) => {
-            // className的处理：value存储的是样式类名
-            if (key === 'className') {
-                ele.className = value;
-                return;
-            }
-            // style的处理：value存储的是样式对象
-            if (key === 'style') {
-                each(value, (val, attr) => {
-                    ele.style[attr] = val;
-                });
-                return;
-            }
-            // 子节点的处理：value存储的children属性值
-            if (key === 'children') {
-                let children = value;
-                if (!Array.isArray(children)) children = [children];
-                children.forEach(child => {
-                    // 子节点是文本节点：直接插入即可
-                    if (/^(string|number)$/.test(typeof child)) {
-                        ele.appendChild(document.createTextNode(child));
-                        return;
-                    }
-                    // 子节点又是一个virtualDOM：递归处理
-                    render(child, ele);
-                });
-                return;
-            }
-            ele.setAttribute(key, value);
+  let { type, props } = virtualDOM;
+  if (typeof type === "string") {
+    // 存储的是标签名:动态创建这样一个标签
+    let ele = document.createElement(type);
+    // 为标签设置相关的属性 & 子节点
+    each(props, (value, key) => {
+      // className的处理：value存储的是样式类名
+      if (key === "className") {
+        ele.className = value;
+        return;
+      }
+      // style的处理：value存储的是样式对象
+      if (key === "style") {
+        each(value, (val, attr) => {
+          ele.style[attr] = val;
         });
-        // 把新增的标签，增加到指定容器中
-        container.appendChild(ele);
-    }
-};
+        return;
+      }
+      // 子节点的处理：value存储的children属性值
+      if (key === "children") {
+        let children = value;
+        if (!Array.isArray(children)) children = [children];
+        children.forEach((child) => {
+          // 子节点是文本节点：直接插入即可
+          if (/^(string|number)$/.test(typeof child)) {
+            ele.appendChild(document.createTextNode(child));
+            return;
+          }
+          // 子节点又是一个virtualDOM：递归处理
+          render(child, ele);
+        });
+        return;
+      }
+      ele.setAttribute(key, value);
+    });
+    // 把新增的标签，增加到指定容器中
+    container.appendChild(ele);
+  }
+}
--- react-systematic-master/01React核心知识/day1123/src/setupProxy.js
+++ react-systematic-master/01React核心知识/day1123/src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-    /* 
+  /* 
     app.use(
         createProxyMiddleware("/api", {
             target: "http://127.0.0.1:7000",
