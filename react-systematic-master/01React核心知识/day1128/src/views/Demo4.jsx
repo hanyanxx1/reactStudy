0e0d3d9bcbd7f25118fc7223ea6d4257867ea24a
hanyanxx1 <hanyanxx1@163.com>
2023-03-10 22:36:34 +0800

    230309 -21.React合成事件语法、22.事件及事件委托

--- react-systematic-master/01React核心知识/day1128/src/views/Demo4.jsx
+++ react-systematic-master/01React核心知识/day1128/src/views/Demo4.jsx
@@ -1,80 +1,122 @@
 import React from "react";
 
 class Demo extends React.Component {
-    render() {
-        return <div className="outer"
-            onClick={() => {
-                console.log('outer 冒泡「合成」');
-            }}
-            onClickCapture={() => {
-                console.log('outer 捕获「合成」');
-            }}>
+  render() {
+    return (
+      <div
+        className="outer"
+        onClick={() => {
+          console.log("outer 冒泡「合成」");
+        }}
+        onClickCapture={() => {
+          console.log("outer 捕获「合成」");
+        }}
+      >
+        <div
+          className="inner"
+          onClick={(ev) => {
+            // ev:合成事件对象
+            console.log("inner 冒泡「合成」", ev, ev.type);
+            // ev.stopPropagation(); //合成事件对象中的“阻止事件传播”:阻止原生的事件传播 & 阻止合成事件中的事件传播
+            // ev.nativeEvent.stopPropagation(); //原生事件对象中的“阻止事件传播”:只能阻止原生事件的传播
+            // ev.nativeEvent.stopImmediatePropagation(); //原生事件对象的阻止事件传播，只不过可以阻止#root上其它绑定的方法执行
 
-            <div className="inner"
-                onClick={(ev) => {
-                    // ev:合成事件对象
-                    console.log('inner 冒泡「合成」', ev, ev.type);
-                    // ev.stopPropagation(); //合成事件对象中的“阻止事件传播”:阻止原生的事件传播 & 阻止合成事件中的事件传播
-                    // ev.nativeEvent.stopPropagation(); //原生事件对象中的“阻止事件传播”:只能阻止原生事件的传播
-                    // ev.nativeEvent.stopImmediatePropagation(); //原生事件对象的阻止事件传播，只不过可以阻止#root上其它绑定的方法执行
-
-                    /* setTimeout(() => {
+            /* setTimeout(() => {
                         console.log(ev, ev.type); //React18中并没有事件对象池机制，所以也不存在：创建的事件对象信息清空问题！！
-                    }, 500); */ 
-                }}
-                onClickCapture={() => {
-                    console.log('inner 捕获「合成」');
-                }}
-            ></div>
+                    }, 500); */
+          }}
+          onClickCapture={() => {
+            console.log("inner 捕获「合成」");
+          }}
+        ></div>
+      </div>
+    );
+  }
 
-        </div>;
-    }
+  componentDidMount() {
+    document.addEventListener(
+      "click",
+      () => {
+        console.log("document 捕获");
+      },
+      true
+    );
+    document.addEventListener(
+      "click",
+      () => {
+        console.log("document 冒泡");
+      },
+      false
+    );
 
-    componentDidMount() {
-        document.addEventListener('click', () => {
-            console.log('document 捕获');
-        }, true);
-        document.addEventListener('click', () => {
-            console.log('document 冒泡');
-        }, false);
+    document.body.addEventListener(
+      "click",
+      () => {
+        console.log("body 捕获");
+      },
+      true
+    );
+    document.body.addEventListener(
+      "click",
+      () => {
+        console.log("body 冒泡");
+      },
+      false
+    );
 
-        document.body.addEventListener('click', () => {
-            console.log('body 捕获');
-        }, true);
-        document.body.addEventListener('click', () => {
-            console.log('body 冒泡');
-        }, false);
+    let root = document.querySelector("#root");
+    root.addEventListener(
+      "click",
+      () => {
+        console.log("root 捕获");
+      },
+      true
+    );
+    root.addEventListener(
+      "click",
+      () => {
+        console.log("root 冒泡");
+      },
+      false
+    );
 
-        let root = document.querySelector('#root');
-        root.addEventListener('click', () => {
-            console.log('root 捕获');
-        }, true);
-        root.addEventListener('click', () => {
-            console.log('root 冒泡');
-        }, false);
+    let outer = document.querySelector(".outer");
+    outer.addEventListener(
+      "click",
+      () => {
+        console.log("outer 捕获「原生」");
+      },
+      true
+    );
+    outer.addEventListener(
+      "click",
+      () => {
+        console.log("outer 冒泡「原生」");
+      },
+      false
+    );
 
-        let outer = document.querySelector('.outer');
-        outer.addEventListener('click', () => {
-            console.log('outer 捕获「原生」');
-        }, true);
-        outer.addEventListener('click', () => {
-            console.log('outer 冒泡「原生」');
-        }, false);
-
-        let inner = document.querySelector('.inner');
-        inner.addEventListener('click', () => {
-            console.log('inner 捕获「原生」');
-        }, true);
-        inner.addEventListener('click', (ev) => {
-            // ev：原生事件对象
-            // ev.stopPropagation();
-            console.log('inner 冒泡「原生」');
-        }, false);
-    }
+    let inner = document.querySelector(".inner");
+    inner.addEventListener(
+      "click",
+      () => {
+        console.log("inner 捕获「原生」");
+      },
+      true
+    );
+    inner.addEventListener(
+      "click",
+      (ev) => {
+        // ev：原生事件对象
+        // ev.stopPropagation();
+        console.log("inner 冒泡「原生」");
+      },
+      false
+    );
+  }
 }
 
 export default Demo;
-
 
 /* 
  React中合成事件的处理原理
@@ -93,4 +135,3 @@ export default Demo;
      原因：因为组件中所渲染的内容，最后都会插入到#root容器中，这样点击页面中任何一个元素，最后都会把#root的点击行为触发！！
      而在给#root绑定的方法中，把之前给元素设置的onXxx/onXxxCapture属性，在相应的阶段执行!!
 */
-
