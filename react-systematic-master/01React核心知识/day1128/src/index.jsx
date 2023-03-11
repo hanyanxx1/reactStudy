7663d8732f66837ad15f6acefacef91cecd9136a
hanyanxx1 <hanyanxx1@163.com>
2023-03-11 19:54:54 +0800

    230311 -25.React事件中的其它细节知识

--- react-systematic-master/01React核心知识/day1128/src/index.jsx
+++ react-systematic-master/01React核心知识/day1128/src/index.jsx
@@ -4,8 +4,8 @@ import Demo from './views/Demo7';
 // import './index.less';
 
 /* 使用FastClick解决移动端使用click事件的300ms延迟问题 */
-// import FastClick from 'fastclick';
-// FastClick.attach(document.body);
+import FastClick from 'fastclick';
+FastClick.attach(document.body);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- react-systematic-master/01React核心知识/day1128/src/views/Demo6.jsx
+++ react-systematic-master/01React核心知识/day1128/src/views/Demo6.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 class Demo extends React.Component {
+  handle = () => {
+    console.log("点击了按钮");
+  };
 
-    handle = () => {
-        console.log('点击了按钮');
-    };
-
-    render() {
-        return <div>
-            <button onClick={this.handle}>
-                提交
-            </button>
-        </div>;
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handle}>提交</button>
+      </div>
+    );
+  }
 }
 
 export default Demo;
