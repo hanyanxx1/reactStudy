83461ac59d47877b94e5e07da72eb9ac6885a5c7
hanyanxx1 <hanyanxx1@163.com>
2023-03-12 22:25:35 +0800

    230311 - 26.TASKOA-启动后台 27.TASKOA-Antd组件库基本应用 28.TASKOA-头部结构样式处理 29.TASKOA-TABLE组件的使用 30.TASKOA-Modal和Form组件的运用 31.TASKOA-数据接口请求的管理\32.TASKOA-整体功能的实现

--- react-systematic-master/01React核心知识/day1129/src/views/Demo.jsx
+++ react-systematic-master/01React核心知识/day1129/src/views/Demo.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 
 class Demo extends React.Component {
-    state = {
-        email: ''
-    };
+  state = {
+    email: "",
+  };
 
-    render() {
-        return <div>
-            <input type="text" value={this.state.email}
-                onChange={(ev) => {
-                    let target = ev.target,
-                        text = target.value.trim();
-                    this.setState({
-                        email: text
-                    });
-                }} />
-        </div>;
-    }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.email}
+          onChange={(ev) => {
+            let target = ev.target,
+              text = target.value.trim();
+            this.setState({
+              email: text,
+            });
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Demo;
