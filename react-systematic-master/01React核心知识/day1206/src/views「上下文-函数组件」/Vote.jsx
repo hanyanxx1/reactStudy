facef69b456a675d380804578746e3f56191bdeb
hanyanxx1 <hanyanxx1@163.com>
2023-03-15 23:55:18 +0800

    230315 -  48.基于上下文方案实现祖先和后代的通信「函数组件」

--- react-systematic-master/01React核心知识/day1206/src/views「上下文-函数组件」/Vote.jsx
+++ react-systematic-master/01React核心知识/day1206/src/views「上下文-函数组件」/Vote.jsx
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
-import './Vote.less';
-import VoteMain from './VoteMain';
-import VoteFooter from './VoteFooter';
+import "./Vote.less";
+import VoteMain from "./VoteMain";
+import VoteFooter from "./VoteFooter";
 import ThemeContext from "../ThemeContext";
 
 const Vote = function Vote() {
-    let [supNum, setSupNum] = useState(10),
-        [oppNum, setOppNum] = useState(5);
-    const change = type => {
-        if (type === 'sup') {
-            setSupNum(supNum + 1);
-            return;
-        }
-        setOppNum(oppNum + 1);
-    };
+  let [supNum, setSupNum] = useState(10),
+    [oppNum, setOppNum] = useState(5);
+  const change = (type) => {
+    if (type === "sup") {
+      setSupNum(supNum + 1);
+      return;
+    }
+    setOppNum(oppNum + 1);
+  };
 
-    return <ThemeContext.Provider
-        value={{
-            supNum,
-            oppNum,
-            change
-        }}>
-        <div className="vote-box">
-            <div className="header">
-                <h2 className="title">React是很棒的前端框架</h2>
-                <span className="num">{supNum + oppNum}</span>
-            </div>
-            <VoteMain />
-            <VoteFooter />
+  return (
+    <ThemeContext.Provider
+      value={{
+        supNum,
+        oppNum,
+        change,
+      }}
+    >
+      <div className="vote-box">
+        <div className="header">
+          <h2 className="title">React是很棒的前端框架</h2>
+          <span className="num">{supNum + oppNum}</span>
         </div>
-    </ThemeContext.Provider>;
+        <VoteMain />
+        <VoteFooter />
+      </div>
+    </ThemeContext.Provider>
+  );
 };
 
 export default Vote;
--- react-systematic-master/01React核心知识/day1206/src/views「上下文-函数组件」/VoteMain.jsx
+++ react-systematic-master/01React核心知识/day1206/src/views「上下文-函数组件」/VoteMain.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import ThemeContext from "../ThemeContext";
 
 const VoteMain = function VoteMain() {
-    let { supNum, oppNum } = useContext(ThemeContext);
-    return <div className="main">
-        <p>支持人数：{supNum}人</p>
-        <p>反对人数：{oppNum}人</p>
-    </div>;
+  let { supNum, oppNum } = useContext(ThemeContext);
+  return (
+    <div className="main">
+      <p>支持人数：{supNum}人</p>
+      <p>反对人数：{oppNum}人</p>
+    </div>
+  );
 };
 
 /* const VoteMain = function VoteMain() {
