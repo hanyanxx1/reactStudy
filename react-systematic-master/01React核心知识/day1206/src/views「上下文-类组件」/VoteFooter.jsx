697d0bf6c3a803fe847cd1370243b0d316f52fff
hanyanxx1 <hanyanxx1@163.com>
2023-03-15 23:29:09 +0800

    230315 -  47.基于上下文方案实现祖先和后代的通信「类组件」

--- react-systematic-master/01React核心知识/day1206/src/views「上下文-类组件」/VoteFooter.jsx
+++ react-systematic-master/01React核心知识/day1206/src/views「上下文-类组件」/VoteFooter.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Button } from 'antd';
+import { Button } from "antd";
 import ThemeContext from "@/ThemeContext";
 
 class VoteFooter extends React.Component {
-    render() {
-        return <ThemeContext.Consumer>
-            {context => {
-                let { change } = context;
-                return <div className="footer">
-                    <Button type="primary" onClick={change.bind(null, 'sup')}>支持</Button>
-                    <Button type="primary" danger onClick={change.bind(null, 'opp')}>反对</Button>
-                </div>;
-            }}
-        </ThemeContext.Consumer>;
-    }
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          let { change } = context;
+          return (
+            <div className="footer">
+              <Button type="primary" onClick={change.bind(null, "sup")}>
+                支持
+              </Button>
+              <Button type="primary" danger onClick={change.bind(null, "opp")}>
+                反对
+              </Button>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
 
 export default VoteFooter;
--- react-systematic-master/01React核心知识/day1206/src/views「上下文-类组件」/VoteMain.jsx
+++ react-systematic-master/01React核心知识/day1206/src/views「上下文-类组件」/VoteMain.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ThemeContext from "@/ThemeContext";
 
 class VoteMain extends React.Component {
-    static contextType = ThemeContext;
-    render() {
-        let { supNum, oppNum } = this.context;
-        return <div className="main">
-            <p>支持人数：{supNum}人</p>
-            <p>反对人数：{oppNum}人</p>
-        </div>;
-    }
+  static contextType = ThemeContext;
+  render() {
+    let { supNum, oppNum } = this.context;
+    return (
+      <div className="main">
+        <p>支持人数：{supNum}人</p>
+        <p>反对人数：{oppNum}人</p>
+      </div>
+    );
+  }
 }
 
 export default VoteMain;
