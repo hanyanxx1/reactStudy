7069730d96f13c22d3e86339480e65dd7f8e19b3
hanyanxx1 <hanyanxx1@163.com>
2023-03-17 17:24:17 +0800

    230317 - 62.react-redux的基础运用

--- react-systematic-master/01React核心知识/day1209/src/index.jsx
+++ react-systematic-master/01React核心知识/day1209/src/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Vote from './views/Vote';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Vote from "./views/Vote";
 /* 使用ANTD组件库 */
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import './index.less';
+import { ConfigProvider } from "antd";
+import zhCN from "antd/locale/zh_CN";
+import "./index.less";
 /* REDUX */
-import store from './store';
-import { Provider } from 'react-redux';
+import store from "./store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ConfigProvider locale={zhCN}>
-        <Provider store={store}>
-            <Vote />
-        </Provider>
-    </ConfigProvider>
+  <ConfigProvider locale={zhCN}>
+    <Provider store={store}>
+      <Vote />
+    </Provider>
+  </ConfigProvider>
 );
--- react-systematic-master/01React核心知识/day1209/src/views/VoteFooter.jsx
+++ react-systematic-master/01React核心知识/day1209/src/views/VoteFooter.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { Button } from 'antd';
+import { Button } from "antd";
 import action from "../store/actions";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 const VoteFooter = function VoteFooter(props) {
-    let { support, oppose } = props;
-    return <div className="footer">
-        <Button type="primary" onClick={support}>支持</Button>
-        <Button type="primary" danger onClick={oppose}>反对</Button>
-    </div >;
+  let { support, oppose } = props;
+  return (
+    <div className="footer">
+      <Button type="primary" onClick={support}>
+        支持
+      </Button>
+      <Button type="primary" danger onClick={oppose}>
+        反对
+      </Button>
+    </div>
+  );
 };
-export default connect(
-    null,
-    action.vote
-)(VoteFooter);
+export default connect(null, action.vote)(VoteFooter);
 
 /* export default connect(
     null,
