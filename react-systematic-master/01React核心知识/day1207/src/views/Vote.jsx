3afc373e0b186938432f9613d554f09064898d4d
hanyanxx1 <hanyanxx1@163.com>
2023-03-17 10:25:12 +0800

    230316 - 55.Redux的基础操作和思想2

--- react-systematic-master/01React核心知识/day1207/src/views/Vote.jsx
+++ react-systematic-master/01React核心知识/day1207/src/views/Vote.jsx
@@ -1,15 +1,15 @@
 import React, { useContext, useState, useEffect } from "react";
-import './Vote.less';
-import VoteMain from './VoteMain';
-import VoteFooter from './VoteFooter';
+import "./Vote.less";
+import VoteMain from "./VoteMain";
+import VoteFooter from "./VoteFooter";
 import ThemeContext from "../ThemeContext";
 
 const Vote = function Vote() {
-    const { store } = useContext(ThemeContext);
-    // 获取容器中的公共状态
-    let { supNum, oppNum } = store.getState();
+  const { store } = useContext(ThemeContext);
+  // 获取容器中的公共状态
+  let { supNum, oppNum } = store.getState();
 
-    /* // 组件第一次渲染完毕后，把让组件更新的方法，放在STORE的事件池中
+  /* // 组件第一次渲染完毕后，把让组件更新的方法，放在STORE的事件池中
     let [num, setNum] = useState(0);
     const update = () => {
         setNum(num + 1);
@@ -23,21 +23,23 @@ const Vote = function Vote() {
             unsubscribe();
         };
     }, [num]); */
-    let [_, setNum] = useState(0);
-    useEffect(() => {
-        store.subscribe(() => {
-            setNum(+new Date());
-        });
-    }, []);
+  let [_, setNum] = useState(0);
+  useEffect(() => {
+    store.subscribe(() => {
+      setNum(+new Date());
+    });
+  }, []);
 
-    return <div className="vote-box">
-        <div className="header">
-            <h2 className="title">React是很棒的前端框架</h2>
-            <span className="num">{supNum + oppNum}</span>
-        </div>
-        <VoteMain />
-        <VoteFooter />
-    </div>;
+  return (
+    <div className="vote-box">
+      <div className="header">
+        <h2 className="title">React是很棒的前端框架</h2>
+        <span className="num">{supNum + oppNum}</span>
+      </div>
+      <VoteMain />
+      <VoteFooter />
+    </div>
+  );
 };
 
 export default Vote;
--- react-systematic-master/01React核心知识/day1207/src/views/VoteMain.jsx
+++ react-systematic-master/01React核心知识/day1207/src/views/VoteMain.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import ThemeContext from "../ThemeContext";
 
 class VoteMain extends React.Component {
-    static contextType = ThemeContext;
+  static contextType = ThemeContext;
 
-    render() {
-        const { store } = this.context;
-        let { supNum, oppNum } = store.getState();
+  render() {
+    const { store } = this.context;
+    let { supNum, oppNum } = store.getState();
 
-        return <div className="main">
-            <p>支持人数：{supNum}人</p>
-            <p>反对人数：{oppNum}人</p>
-        </div>;
-    }
+    return (
+      <div className="main">
+        <p>支持人数：{supNum}人</p>
+        <p>反对人数：{oppNum}人</p>
+      </div>
+    );
+  }
 
-    componentDidMount() {
-        const { store } = this.context;
-        store.subscribe(() => {
-            this.forceUpdate();
-        });
-    }
+  componentDidMount() {
+    const { store } = this.context;
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
 }
 
 export default VoteMain;
