763cca71953b6e35e2b67fc474b013a1f2071717
hanyanxx1 <hanyanxx1@163.com>
2023-03-19 18:21:16 +0800

    230319 - 77.react-router-dom的基础运用和细节

--- react-systematic-master/01React核心知识/day1219/src「路由基础知识」/App.jsx
+++ react-systematic-master/01React核心知识/day1219/src「路由基础知识」/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { HashRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect, Link } from "react-router-dom";
 
 /* 导入组件 */
-import A from './views/A';
-import B from './views/B';
-import C from './views/C';
+import A from "./views/A";
+import B from "./views/B";
+import C from "./views/C";
 
 /* 导航区域的样式 */
 import styled from "styled-components";
 const NavBox = styled.nav`
-   a{
+  a {
     margin-right: 10px;
     color: #000;
-   }
+  }
 `;
 
 const App = function App() {
-    /* 
+  /* 
     基于<HashRouter>把所有要渲染的内容包起来，开启HASH路由 
       + 后续用到的<Route>、<Link>等，都需要在HashRouter/BrowserRouter中使用
       + 开启后，整个页面地址，默认会设置一个 #/ 哈希值
@@ -25,33 +25,37 @@ const App = function App() {
       + 最后渲染完毕的结果依然是A标签
       + 它可以根据路由模式，自动设定点击A切换的方式
     */
-    return <HashRouter>
-        {/* 导航部分 */}
-        <NavBox>
-            <Link to="/">A</Link>
-            <Link to="/b">B</Link>
-            <Link to="/c">C</Link>
-        </NavBox>
+  return (
+    <HashRouter>
+      {/* 导航部分 */}
+      <NavBox>
+        <Link to="/">A</Link>
+        <Link to="/b">B</Link>
+        <Link to="/c">C</Link>
+      </NavBox>
 
-        {/* 路由容器:每一次页面加载或者路由切换完毕，都会根据当前的哈希值，到这里和每一个Route进行匹配，把匹配到的组件，放在容器中渲染！！ */}
-        <div className="content">
-            {/* 
+      {/* 路由容器:每一次页面加载或者路由切换完毕，都会根据当前的哈希值，到这里和每一个Route进行匹配，把匹配到的组件，放在容器中渲染！！ */}
+      <div className="content">
+        {/* 
              Switch：确保路由中，只要有一项匹配，则不再继续向下匹配
              exact：设置匹配模式为精准匹配
              */}
-            <Switch>
-                <Route exact path="/" component={A} />
-                <Route path="/b" component={B} />
-                <Route path="/c" render={() => {
-                    // 当路由地址匹配后，先把render函数执行，返回的返回值就是我们需要渲染的内容
-                    // 在此函数中，可以处理一些事情，例如：登录态检验....
-                    let isLogin = true;
-                    if (isLogin) {
-                        return <C />;
-                    }
-                    return <Redirect to="/login" />
-                }} />
-                {/* 
+        <Switch>
+          <Route exact path="/" component={A} />
+          <Route path="/b" component={B} />
+          <Route
+            path="/c"
+            render={() => {
+              // 当路由地址匹配后，先把render函数执行，返回的返回值就是我们需要渲染的内容
+              // 在此函数中，可以处理一些事情，例如：登录态检验....
+              let isLogin = true;
+              if (isLogin) {
+                return <C />;
+              }
+              return <Redirect to="/login" />;
+            }}
+          />
+          {/* 
                   // 放在最后一项，path设置※或者不写，意思是：以上都不匹配，则执行这个规则
                   <Route path="*" component={404组件} /> 
                   // 当然也可以不设置404组件，而是重定向到默认 / 地址：
@@ -60,10 +64,11 @@ const App = function App() {
                     + to:重定向的地址
                     + exact是对from地址的修饰，开启精准匹配
                 */}
-                <Redirect to="/" />
-            </Switch>
-        </div>
-    </HashRouter>;
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
 };
 
 export default App;
