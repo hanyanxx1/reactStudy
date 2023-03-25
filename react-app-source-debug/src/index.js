65261a77a832ec5458b55cf04fd3e660b73881dd
hanyanxx1 <hanyanxx1@163.com>
2023-03-25 17:39:06 +0800

    230325 - react-app-source-debug React 之如何调试源码 https://juejin.cn/post/7168821587251036167

---
+++ react-app-source-debug/src/index.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<div>react debug</div>);
