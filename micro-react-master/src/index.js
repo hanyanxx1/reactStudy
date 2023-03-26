c0d8985841f1822830f4b5d5bccdc46cd9acb7f6
hanyanxx1 <hanyanxx1@163.com>
2023-03-26 10:28:40 +0800

    230326 - micro-react-master - 1.createElement

--- micro-react-master/src/index.js
+++ micro-react-master/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>111</div>);
+import { createElement } from "./micro-react";
 
+const element = createElement(
+  "h1",
+  { id: "title", class: "hello" },
+  "Hello world",
+  createElement("h2")
+);
+
+console.log(element);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
