b6fc632230040bebd281d7919c15406bef46daf7
hanyanxx1 <hanyanxx1@163.com>
2023-03-26 19:50:34 +0800

    230326 - micro-react-master - 4.Fibers

--- micro-react-master/src/index.js
+++ micro-react-master/src/index.js
@@ -1,10 +1,27 @@
 import { createElement, render } from "./micro-react";
 
+// const element = createElement(
+//   "h1",
+//   { id: "title", class: "hello" },
+//   "Hello world",
+//   createElement("h2")
+// );
+
 const element = createElement(
-  "h1",
-  { id: "title", class: "hello" },
-  "Hello world",
-  createElement("h2")
+  "div",
+  { id: "top" },
+  createElement(
+    "div",
+    { id: "divA" },
+    createElement("span", { id: "spanA1" }, "A1"),
+    createElement("span", { id: "spanA2" }, "A2")
+  ),
+  createElement(
+    "div",
+    { id: "divB" },
+    createElement("span", { id: "spanB1" }, "B1"),
+    createElement("span", { id: "spanB2" }, "B2")
+  )
 );
 
 render(element, document.getElementById("root"));
