e45029763e32efa4f27973ed0b38194636e37b7f
hanyanxx1 <hanyanxx1@163.com>
2023-03-26 23:29:29 +0800

    230326 - micro-react-master - 6.Reconciliation

--- micro-react-master/src/index.js
+++ micro-react-master/src/index.js
@@ -1,27 +1,42 @@
 import { createElement, render } from "./micro-react";
 
 // const element = createElement(
-//   "h1",
-//   { id: "title", class: "hello" },
-//   "Hello world",
-//   createElement("h2")
+//   "div",
+//   { id: "top" },
+//   createElement(
+//     "div",
+//     { id: "divA" },
+//     createElement("span", { id: "spanA1" }, "A1"),
+//     createElement("span", { id: "spanA2" }, "A2")
+//   ),
+//   createElement(
+//     "div",
+//     { id: "divB" },
+//     createElement("span", { id: "spanB1" }, "B1"),
+//     createElement("span", { id: "spanB2" }, "B2")
+//   )
 // );
 
-const element = createElement(
-  "div",
-  { id: "top" },
-  createElement(
-    "div",
-    { id: "divA" },
-    createElement("span", { id: "spanA1" }, "A1"),
-    createElement("span", { id: "spanA2" }, "A2")
-  ),
-  createElement(
-    "div",
-    { id: "divB" },
-    createElement("span", { id: "spanB1" }, "B1"),
-    createElement("span", { id: "spanB2" }, "B2")
-  )
-);
+const handleChange = (e) => {
+  renderer(e.target.value);
+};
 
-render(element, document.getElementById("root"));
+const container = document.querySelector('#root');
+
+const renderer = (value) => {
+  const element = createElement(
+    "div",
+    null,
+    createElement("input", {
+      value: value,
+      oninput: (e) => {
+        handleChange(e);
+      },
+    }),
+    createElement("h2", null, value)
+  );
+  render(element, container);
+};
+
+renderer('Hello');
+
--- micro-react-master/src/micro-react/render.js
+++ micro-react-master/src/micro-react/render.js
@@ -15,8 +15,46 @@ function createDOM(fiber) {
   return dom;
 }
 
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+function updateDom(dom, prevProps, nextProps) {
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 function commitRoot() {
+  deletions.forEach(commitWork);
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 }
 
@@ -26,7 +64,13 @@ function commitWork(fiber) {
   }
 
   const parentDOM = fiber.parent.dom;
-  parentDOM.append(fiber.dom);
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    parentDOM.append(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    parentDOM.removeChild(fiber.dom);
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
@@ -37,12 +81,16 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
 let nextUnitOfWork = null;
 let wipRoot = null;
+let currentRoot = null;
+let deletions = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -66,28 +114,7 @@ function performUnitOfWork(fiber) {
   }
 
   const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-
-  while (index < elements.length) {
-    const element = elements[index];
-
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-
-    prevSibling = newFiber;
-    index++;
-  }
+  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -98,6 +125,58 @@ function performUnitOfWork(fiber) {
       return nextFiber.sibling;
     }
     nextFiber = nextFiber.parent;
+  }
+}
+
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    const sameType = oldFiber && element && element.type === oldFiber.type;
+    let newFiber = null;
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+
+    index++;
   }
 }
 
