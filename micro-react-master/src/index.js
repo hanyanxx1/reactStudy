727a59f4a40119f24e90070277696cbdfc0758a8
hanyanxx1 <hanyanxx1@163.com>
2023-03-27 09:55:00 +0800

    230327 - micro-react-master - 7.Function Components

--- micro-react-master/src/index.js
+++ micro-react-master/src/index.js
@@ -17,26 +17,41 @@ import { createElement, render } from "./micro-react";
 //   )
 // );
 
-const handleChange = (e) => {
-  renderer(e.target.value);
+// const handleChange = (e) => {
+//   renderer(e.target.value);
+// };
+
+const container = document.querySelector("#root");
+
+// const renderer = (value) => {
+//   const element = createElement(
+//     "div",
+//     null,
+//     createElement("input", {
+//       value: value,
+//       oninput: (e) => {
+//         handleChange(e);
+//       },
+//     }),
+//     createElement("h2", null, value)
+//   );
+//   render(element, container);
+// };
+
+// renderer('Hello');
+
+const App = (props) => {
+  return createElement("h1", null, "Hi", props.name);
 };
 
-const container = document.querySelector('#root');
+// const Counter = () => {
+//   const [state, setState] = useState(1);
+//   return createElement(
+//     'h1',
+//     { onclick: () => setState((prev) => prev + 1) },
+//     state
+//   );
+// };
 
-const renderer = (value) => {
-  const element = createElement(
-    "div",
-    null,
-    createElement("input", {
-      value: value,
-      oninput: (e) => {
-        handleChange(e);
-      },
-    }),
-    createElement("h2", null, value)
-  );
-  render(element, container);
-};
-
-renderer('Hello');
-
+const element = createElement(App, { name: "kelvin" });
+render(element, container);
--- micro-react-master/src/micro-react/render.js
+++ micro-react-master/src/micro-react/render.js
@@ -63,16 +63,29 @@ function commitWork(fiber) {
     return;
   }
 
-  const parentDOM = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+  const parentDOM = domParentFiber.dom;
+
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
-    parentDOM.append(fiber.dom);
+    parentDOM.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   } else if (fiber.effectTag === "DELETION") {
-    parentDOM.removeChild(fiber.dom);
+    commitDeletion(fiber, parentDOM)
   }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child, domParent)
+  }
 }
 
 function render(element, container) {
@@ -109,12 +122,12 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDOM(fiber);
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
-
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -126,6 +139,18 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
+}
+
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
+  reconcileChildren(fiber, fiber.props.children);
 }
 
 function reconcileChildren(wipFiber, elements) {
