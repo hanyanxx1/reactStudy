e546f80e3d6c6320f8d7b050a15a4381135fa555
hanyanxx1 <hanyanxx1@163.com>
2023-03-31 17:59:23 +0800

    230330 - react-18 - 9.prepareFreshStack

--- zf-react18/react182/src/react-reconciler/src/ReactFiber.js
+++ zf-react18/react182/src/react-reconciler/src/ReactFiber.js
@@ -28,3 +28,33 @@ function createFiber(tag, pendingProps, key) {
 export function createHostRootFiber() {
   return createFiber(HostRoot, null, null);
 }
+// We use a double buffering pooling technique because we know that we'll
+// only ever need at most two versions of a tree. We pool the "other" unused
+// node that we're free to reuse. This is lazily created to avoid allocating
+// extra objects for things that are never updated. It also allow us to
+// reclaim the extra memory if needed.
+//我们使用双缓冲池技术，因为我们知道一棵树最多只需要两个版本
+//我们将“其他”未使用的我们可以自由重用的节点
+//这是延迟创建的，以避免分配从未更新的内容的额外对象。它还允许我们如果需要，回收额外的内+存kd
+export function createWorkInProgress(current, pendingProps) {
+  let workInProgress = current.alternate;
+  if (workInProgress === null) {
+    workInProgress = createFiber(current.tag, pendingProps, current.key);
+    workInProgress.type = current.type;
+    workInProgress.stateNode = current.stateNode;
+    workInProgress.alternate = current;
+    current.alternate = workInProgress;
+  } else {
+    workInProgress.pendingProps = pendingProps;
+    workInProgress.type = current.type;
+    workInProgress.flags = NoFlags;
+    workInProgress.subtreeFlags = NoFlags;
+  }
+  workInProgress.child = current.child;
+  workInProgress.memoizedProps = current.memoizedProps;
+  workInProgress.memoizedState = current.memoizedState;
+  workInProgress.updateQueue = current.updateQueue;
+  workInProgress.sibling = current.sibling;
+  workInProgress.index = current.index;
+  return workInProgress;
+}
--- zf-react18/react182/src/react-reconciler/src/ReactFiberWorkLoop.js
+++ zf-react18/react182/src/react-reconciler/src/ReactFiberWorkLoop.js
@@ -1,4 +1,7 @@
 import { scheduleCallback } from "scheduler";
+import { createWorkInProgress } from "./ReactFiber";
+
+let workInProgress = null;
 export function scheduleUpdateOnFiber(root) {
   ensureRootIsScheduled(root);
 }
@@ -6,5 +9,12 @@ function ensureRootIsScheduled(root) {
   scheduleCallback(performConcurrentWorkOnRoot.bind(null, root));
 }
 function performConcurrentWorkOnRoot(root) {
-  console.log("performConcurrentWorkOnRoot");
+  renderRootSync(root);
+}
+function prepareFreshStack(root) {
+  workInProgress = createWorkInProgress(root.current, null);
+  console.log(workInProgress);
+}
+function renderRootSync(root) {
+  prepareFreshStack(root);
 }
