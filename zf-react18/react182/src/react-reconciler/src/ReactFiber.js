de31a437a32d83a3200b8d13efadd884c81e1b3c
hanyanxx1 <hanyanxx1@163.com>
2023-03-31 16:52:49 +0800

    230330 - react-18 - 5.创建 RootFiber

---
+++ zf-react18/react182/src/react-reconciler/src/ReactFiber.js
@@ -0,0 +1,30 @@
+import { HostRoot } from "./ReactWorkTags";
+import { NoFlags } from "./ReactFiberFlags";
+
+export function FiberNode(tag, pendingProps, key) {
+  this.tag = tag;
+  this.key = key;
+  this.type = null;
+  this.stateNode = null;
+
+  this.return = null;
+  this.child = null;
+  this.sibling = null;
+
+  this.pendingProps = pendingProps;
+  this.memoizedProps = null;
+  this.updateQueue = null;
+  this.memoizedState = null;
+
+  this.flags = NoFlags;
+  this.subtreeFlags = NoFlags;
+  this.alternate = null;
+}
+
+function createFiber(tag, pendingProps, key) {
+  return new FiberNode(tag, pendingProps, key);
+}
+
+export function createHostRootFiber() {
+  return createFiber(HostRoot, null, null);
+}
