facea8db9a76086b48c347e3f9de13cd8a4af9e6
hanyanxx1 <hanyanxx1@163.com>
2023-04-01 23:40:34 +0800

    230401 - react-18 - 16.注册事件名

--- zf-react18/react182/src/main.jsx
+++ zf-react18/react182/src/main.jsx
@@ -5,13 +5,32 @@ import { createRoot } from "react-dom/client";
 //   </h1>
 // );
 
+// function FunctionComponent() {
+//   return (
+//     <h1>
+//       hello<span style={{ color: "red" }}>world</span>
+//     </h1>
+//   );
+// }
+
 function FunctionComponent() {
   return (
-    <h1>
-      hello<span style={{ color: "red" }}>world</span>
+    <h1
+      onClick={() => console.log("onClick FunctionComponent")}
+      onClickCapture={() => console.log("onClickCapture FunctionComponent")}
+    >
+      hello
+      <span
+        style={{ color: "red" }}
+        onClick={() => console.log("onClick span")}
+        onClickCapture={() => console.log("onClickCapture span")}
+      >
+        world
+      </span>
     </h1>
   );
 }
+
 let element = <FunctionComponent />;
 
 const root = createRoot(document.getElementById("root"));
---
+++ zf-react18/react182/src/react-dom-bindings/src/events/DOMEventProperties.js
@@ -0,0 +1,14 @@
+import { registerTwoPhaseEvent } from "./EventRegistry";
+
+const simpleEventPluginEvents = ["click"];
+function registerSimpleEvent(domEventName, reactName) {
+  registerTwoPhaseEvent(reactName, [domEventName]);
+}
+export function registerSimpleEvents() {
+  for (let i = 0; i < simpleEventPluginEvents.length; i++) {
+    const eventName = simpleEventPluginEvents[i]; // click
+    const domEventName = eventName.toLowerCase(); // click
+    const capitalizedEvent = eventName[0].toUpperCase() + eventName.slice(1);
+    registerSimpleEvent(domEventName, `on${capitalizedEvent}`); // click=>onClick
+  }
+}
