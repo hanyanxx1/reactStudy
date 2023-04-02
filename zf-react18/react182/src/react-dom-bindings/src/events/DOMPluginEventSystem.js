aece09119c3865dc2e49f5290c0031a5bddb8cd2
hanyanxx1 <hanyanxx1@163.com>
2023-04-02 11:01:02 +0800

    230401 - react-18 - 17.listenToNativeEvent

--- zf-react18/react182/src/react-dom-bindings/src/events/DOMPluginEventSystem.js
+++ zf-react18/react182/src/react-dom-bindings/src/events/DOMPluginEventSystem.js
@@ -1,8 +1,48 @@
 import { allNativeEvents } from "./EventRegistry";
 import * as SimpleEventPlugin from "./plugins/SimpleEventPlugin";
+import { createEventListenerWrapperWithPriority } from "./ReactDOMEventListener";
+import { IS_CAPTURE_PHASE } from "./EventSystemFlags";
+import { addEventCaptureListener, addEventBubbleListener } from "./EventListener";
+
 SimpleEventPlugin.registerEvents();
 export function listenToAllSupportedEvents(rootContainerElement) {
   allNativeEvents.forEach((domEventName) => {
-    console.log(domEventName);
+    listenToNativeEvent(domEventName, true, rootContainerElement);
+    listenToNativeEvent(domEventName, false, rootContainerElement);
   });
+}
+
+export function listenToNativeEvent(
+  domEventName,
+  isCapturePhaseListener,
+  target
+) {
+  let eventSystemFlags = 0; // 冒泡 = 0 捕获 = 4
+  if (isCapturePhaseListener) {
+    eventSystemFlags |= IS_CAPTURE_PHASE;
+  }
+  addTrappedEventListener(
+    target,
+    domEventName,
+    eventSystemFlags,
+    isCapturePhaseListener
+  );
+}
+
+function addTrappedEventListener(
+  targetContainer,
+  domEventName,
+  eventSystemFlags,
+  isCapturePhaseListener
+) {
+  const listener = createEventListenerWrapperWithPriority(
+    targetContainer,
+    domEventName,
+    eventSystemFlags
+  );
+  if (isCapturePhaseListener) {
+    addEventCaptureListener(targetContainer, domEventName, listener);
+  } else {
+    addEventBubbleListener(targetContainer, domEventName, listener);
+  }
 }
