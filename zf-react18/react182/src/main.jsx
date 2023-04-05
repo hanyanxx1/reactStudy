1e1591ff97f6b1e76ef3688d6a721bc1cc5fde6f
hanyanxx1 <hanyanxx1@163.com>
2023-04-05 14:54:59 +0800

    230405 - react-18 - 24.单节点(key 相同,类型相同)

--- zf-react18/react182/src/main.jsx
+++ zf-react18/react182/src/main.jsx
@@ -55,13 +55,39 @@ import { createRoot } from "react-dom/client";
 //   );
 // }
 
+// function FunctionComponent() {
+//   console.log("FunctionComponent render");
+//   const [number, setNumber] = React.useState(10);
+//   return (
+//     <button onClick={() => setNumber((number) => number + 1)}>{number}</button>
+//   );
+// }
+
 function FunctionComponent() {
-  console.log("FunctionComponent render");
-  const [number, setNumber] = React.useState(10);
-  return (
-    <button onClick={() => setNumber((number) => number + 1)}>{number}</button>
+  const [number, setNumber] = React.useState(0);
+  return number === 0 ? (
+    <div onClick={() => setNumber(number + 1)} key="title" id="title">
+      title
+    </div>
+  ) : (
+    <div onClick={() => setNumber(number + 1)} key="title" id="title2">
+      title2
+    </div>
   );
 }
+
+// function FunctionComponent() {
+//   const [number, setNumber] = React.useState(0);
+//   return number === 0 ? (
+//     <div onClick={() => setNumber(number + 1)} key="title1" id="title">
+//       title
+//     </div>
+//   ) : (
+//     <div onClick={() => setNumber(number + 1)} key="title2" id="title2">
+//       title2
+//     </div>
+//   );
+// }
 
 let element = <FunctionComponent />;
 
