3c4ce96cb06ed40bf5418b0a0dc2aa5efc3d96f1
hanyanxx1 <hanyanxx1@163.com>
2023-04-05 17:39:35 +0800

    230405 - react-18 - 29.多个节点的数量和 key 相同，有的 type 不同

--- zf-react18/react182/src/main.jsx
+++ zf-react18/react182/src/main.jsx
@@ -106,7 +106,28 @@ import { createRoot } from "react-dom/client";
 // }
 
 //27.原来多个节点，现在只有一个节点
+// function FunctionComponent() {
+//   const [number, setNumber] = React.useState(0);
+//   return number === 0 ? (
+//     <ul key="container" onClick={() => setNumber(number + 1)}>
+//       <li key="A">A</li>
+//       <li key="B" id="B">
+//         B
+//       </li>
+//       <li key="C">C</li>
+//     </ul>
+//   ) : (
+//     <ul key="container" onClick={() => setNumber(number + 1)}>
+//       <li key="B" id="B2">
+//         B2
+//       </li>
+//     </ul>
+//   );
+// }
+
+//29.多个节点的数量和 key 相同，有的 type 不同
 function FunctionComponent() {
+  console.log("FunctionComponent");
   const [number, setNumber] = React.useState(0);
   return number === 0 ? (
     <ul key="container" onClick={() => setNumber(number + 1)}>
@@ -114,12 +135,18 @@ function FunctionComponent() {
       <li key="B" id="B">
         B
       </li>
-      <li key="C">C</li>
+      <li key="C" id="C">
+        C
+      </li>
     </ul>
   ) : (
     <ul key="container" onClick={() => setNumber(number + 1)}>
-      <li key="B" id="B2">
+      <li key="A">A2</li>
+      <p key="B" id="B2">
         B2
+      </p>
+      <li key="C" id="C2">
+        C2
       </li>
     </ul>
   );
