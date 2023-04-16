072092e3788837d2f4be85d8a83b282c9286987d
hanyanxx1 <hanyanxx1@163.com>
2023-04-17 00:39:55 +0800

    230417 - 2023年最新珠峰React全家桶 - 126.Antd Pro的基本骨架2

---
+++ ant-design-pro-demo/config/defaultSettings.ts
@@ -0,0 +1,19 @@
+/**
+ * @name 对Layout的基础配置
+ */
+const Settings = {
+  title: 'CMS管理系统',
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  navTheme: 'light',
+  // 拂晓蓝
+  colorPrimary: '#1890ff',
+  colorWeak: false,
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export default Settings;
