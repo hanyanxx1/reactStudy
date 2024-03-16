67203e6ed50809727b83de46b4df4beb548c0bb1
hanyanxx1 <hanyanxx1@163.com>
2024-03-16 21:13:26 +0800

    react18-core - 5.创建 RootFiber

--- React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-reconciler/src/ReactFiberRoot.js
+++ React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-reconciler/src/ReactFiberRoot.js
@@ -1,8 +1,13 @@
+import { createHostRootFiber } from "./ReactFiber";
+
 function FiberRootNode(containerInfo) {
   this.containerInfo = containerInfo;
 }
 
 export function createFiberRoot(containerInfo) {
   const root = new FiberRootNode(containerInfo);
+  const uninitializedFiber = createHostRootFiber();
+  root.current = uninitializedFiber;
+  uninitializedFiber.stateNode = root;
   return root;
 }
