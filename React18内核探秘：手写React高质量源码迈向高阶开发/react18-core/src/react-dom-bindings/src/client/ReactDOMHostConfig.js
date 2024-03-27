cbd6cb66b854f4c656804671d3cfb5fd055b8a80
hanyanxx1 <hanyanxx1@163.com>
2024-03-27 09:48:04 +0800

    react18-core - 11.completeUnitOfWork

--- React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-dom-bindings/src/client/ReactDOMHostConfig.js
+++ React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-dom-bindings/src/client/ReactDOMHostConfig.js
@@ -1,3 +1,20 @@
+import { setInitialProperties } from "./ReactDOMComponent";
+
 export function shouldSetTextContent(type, props) {
   return typeof props.children === "string" || typeof props.children === "number";
+}
+
+export const appendInitialChild = (parent, child) => {
+  parent.appendChild(child);
+};
+
+export const createInstance = (type, props, internalInstanceHandle) => {
+  const domElement = document.createElement(type);
+  return domElement;
+};
+
+export const createTextInstance = (content) => document.createTextNode(content);
+
+export function finalizeInitialChildren(domElement, type, props) {
+  setInitialProperties(domElement, type, props);
 }
