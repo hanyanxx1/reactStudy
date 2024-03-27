a265605ebca458f4056c66cd1b3dd1ea71a4a730
hanyanxx1 <hanyanxx1@163.com>
2024-03-27 09:57:41 +0800

    react18-core - 12.commitRoot

--- React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-reconciler/src/ReactFiberWorkLoop.js
+++ React18内核探秘：手写React高质量源码迈向高阶开发/react18-core/src/react-reconciler/src/ReactFiberWorkLoop.js
@@ -2,6 +2,8 @@ import { scheduleCallback } from "scheduler";
 import { createWorkInProgress } from "./ReactFiber";
 import { beginWork } from "./ReactFiberBeginWork";
 import { completeWork } from "./ReactFiberCompleteWork";
+import { HostComponent, HostRoot, HostText, FunctionComponent } from "./ReactWorkTags";
+import { NoFlags, Placement, Update, ChildDeletion, MutationMask } from "./ReactFiberFlags";
 
 let workInProgress = null;
 export function scheduleUpdateOnFiber(root) {
@@ -12,7 +14,87 @@ function ensureRootIsScheduled(root) {
 }
 function performConcurrentWorkOnRoot(root) {
   renderRootSync(root);
+  const finishedWork = root.current.alternate;
+  printFiber(finishedWork);
+  console.log(finishedWork);
+  console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+  root.finishedWork = finishedWork;
+  commitRoot(root);
 }
+
+function commitRoot(root) {
+  const { finishedWork } = root;
+  const subtreeHasEffects = (finishedWork.subtreeFlags & MutationMask) !== NoFlags;
+  const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags;
+  if (subtreeHasEffects || rootHasEffect) {
+    console.log("commitRoot");
+  }
+  root.current = finishedWork;
+}
+
+function printFiber(fiber) {
+  /*
+  fiber.flags &= ~Forked;
+  fiber.flags &= ~PlacementDEV;
+  fiber.flags &= ~Snapshot;
+  fiber.flags &= ~PerformedWork;
+  */
+  if (fiber.flags !== 0) {
+    console.log(
+      getFlags(fiber.flags),
+      getTag(fiber.tag),
+      typeof fiber.type === "function" ? fiber.type.name : fiber.type,
+      fiber.memoizedProps
+    );
+    if (fiber.deletions) {
+      for (let i = 0; i < fiber.deletions.length; i++) {
+        const childToDelete = fiber.deletions[i];
+        console.log(getTag(childToDelete.tag), childToDelete.type, childToDelete.memoizedProps);
+      }
+    }
+  }
+  let child = fiber.child;
+  while (child) {
+    printFiber(child);
+    child = child.sibling;
+  }
+}
+function getTag(tag) {
+  switch (tag) {
+    case FunctionComponent:
+      return `FunctionComponent`;
+    case HostRoot:
+      return `HostRoot`;
+    case HostComponent:
+      return `HostComponent`;
+    case HostText:
+      return HostText;
+    default:
+      return tag;
+  }
+}
+function getFlags(flags) {
+  if (flags === (Update | Placement | ChildDeletion)) {
+    return `自己移动和子元素有删除`;
+  }
+  if (flags === (ChildDeletion | Update)) {
+    return `自己有更新和子元素有删除`;
+  }
+  if (flags === ChildDeletion) {
+    return `子元素有删除`;
+  }
+  if (flags === (Placement | Update)) {
+    return `移动并更新`;
+  }
+  if (flags === Placement) {
+    return `插入`;
+  }
+  if (flags === Update) {
+    return `更新`;
+  }
+  return flags;
+}
+
 function prepareFreshStack(root) {
   workInProgress = createWorkInProgress(root.current, null);
 }
