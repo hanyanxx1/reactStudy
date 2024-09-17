a28484d4ac348f164be25b2707ec3e3598025818
hanyanxx1 <hanyanxx1@163.com>
2024-09-17 11:26:21 +0800

    feat:  18.6 数据共享

--- react核心及周边/useRequest-master/src/App.jsx
+++ react核心及周边/useRequest-master/src/App.jsx
@@ -1,55 +1,36 @@
-import { useState } from "react";
-import { useRequest, clearCache } from "./ahooks";
+import { useRequest } from "ahooks";
 let counter = 0;
-function getName(keyword = "") {
+function getName() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
         time: new Date().toLocaleTimeString(),
-        data: keyword + ++counter,
+        data: "zhufeng" + ++counter,
       });
     }, 2000);
   });
 }
 function User() {
-  const { data, loading, params, run } = useRequest(getName, {
+  const { data, loading, refresh } = useRequest(getName, {
     cacheKey: "cacheKey",
-    staleTime: 5000,
-    setCache: (data) => localStorage.setItem("cacheKey", JSON.stringify(data)),
-    getCache: () => JSON.parse(localStorage.getItem("cacheKey") || "{}"),
   });
-  const [keyword, setKeyword] = useState(params[0] || "");
-  if (!data && loading) {
-    return <p>加载中...</p>;
-  }
   return (
     <>
-      <div>
-        <input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-        <button
-          onClick={() => {
-            run(keyword);
-          }}
-        >
-          获取用户名
-        </button>
-      </div>
-      <button onClick={() => clearCache("cacheKey")}>清除缓存</button>
       <p>后台加载中: {loading ? "true" : "false"}</p>
+      <button onClick={refresh} type="button">
+        更新
+      </button>
       <p>最近的请求时间: {data?.time}</p>
-      <p>Keyword: {keyword}</p>
       <p>{data?.data}</p>
     </>
   );
 }
 function App() {
-  const [visible, setVisible] = useState(true);
   return (
     <div>
-      <button type="button" onClick={() => setVisible(!visible)}>
-        {visible ? "隐藏" : "显示"}
-      </button>
-      {visible && <User />}
+      <User />
+      <hr />
+      <User />
     </div>
   );
 }
--- react核心及周边/useRequest-master/src/ahooks/useRequest/src/plugins/useCachePlugin.js
+++ react核心及周边/useRequest-master/src/ahooks/useRequest/src/plugins/useCachePlugin.js
@@ -1,25 +1,35 @@
-import useCreation from "../../../useCreation";
+import { useRef } from "react";
 import * as cache from "../utils/cache";
-
+import useCreation from "../../../useCreation";
+import * as cachePromise from "../utils/cachePromise";
+import * as cacheSubscribe from "../utils/cacheSubscribe";
 const useCachePlugin = (
   fetchInstance,
-  { cacheKey, staleTime = 0, setCache: customSetCache, getCache: customGetCache }
+  {
+    cacheKey,
+    staleTime = 0,
+    cacheTime = 5 * 60 * 1000,
+    setCache: customSetCache,
+    getCache: customGetCache,
+  }
 ) => {
+  const unSubscribeRef = useRef();
+  const currentPromiseRef = useRef();
   const _setCache = (key, cachedData) => {
     if (customSetCache) {
-      customSetCache(key, cachedData);
+      customSetCache(cachedData);
     } else {
-      cache.setCache(key, cachedData);
+      cache.setCache(key, cacheTime, cachedData);
     }
+    cacheSubscribe.trigger(key, cachedData.data);
   };
 
-  const _getCache = (key) => {
+  const _getCache = (key, params) => {
     if (customGetCache) {
-      return customGetCache(key);
+      return customGetCache(params);
     }
     return cache.getCache(key);
   };
-
   useCreation(() => {
     if (!cacheKey) {
       return;
@@ -33,7 +43,6 @@ const useCachePlugin = (
       }
     }
   });
-
   if (!cacheKey) {
     return {};
   }
@@ -56,6 +65,20 @@ const useCachePlugin = (
         };
       }
     },
+    onRequest: (service, args) => {
+      let servicePromise = cachePromise.getCachePromise(cacheKey);
+      if (servicePromise && servicePromise !== currentPromiseRef.current) {
+        return {
+          servicePromise,
+        };
+      }
+      servicePromise = service(...args);
+      currentPromiseRef.current = servicePromise;
+      cachePromise.setCachePromise(cacheKey, servicePromise);
+      return {
+        servicePromise,
+      };
+    },
     onSuccess: (data, params) => {
       if (cacheKey) {
         _setCache(cacheKey, {
@@ -63,9 +86,13 @@ const useCachePlugin = (
           params,
           time: new Date().getTime(),
         });
+        unSubscribeRef.current = cacheSubscribe.subscribe(cacheKey, (d) => {
+          fetchInstance.setState({
+            data: d,
+          });
+        });
       }
     },
   };
 };
-
 export default useCachePlugin;
