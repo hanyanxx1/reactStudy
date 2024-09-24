bd8ccd5eded9d2b78decf7e95f9969ccb17ea95f
hanyanxx1 <hanyanxx1@163.com>
2024-09-24 09:10:39 +0800

    2.使用基本路由

--- react核心及周边/react-router-v6/src/App.js
+++ react核心及周边/react-router-v6/src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import User from "./components/User";
+import Profile from "./components/Profile";
+
 function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ react核心及周边/react-router-v6/src/components/User.js
@@ -0,0 +1,5 @@
+import React from "react";
+function User() {
+  return <div>User</div>;
+}
+export default User;
