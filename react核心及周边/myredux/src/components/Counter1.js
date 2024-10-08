11e442738c0b5f4724d1266eaf2ff4b6122839ce
hanyanxx1 <hanyanxx1@163.com>
2024-10-08 20:51:39 +0800

    5. React计数器

---
+++ react核心及周边/myredux/src/components/Counter1.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { createStore } from "../redux";
+const ADD = "ADD";
+const MINUS = "MINUS";
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case ADD:
+      return { number: state.number + 1 };
+    case MINUS:
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+};
+let initState = { number: 0 };
+const store = createStore(reducer, initState);
+
+export default class Counter extends Component {
+  unsubscribe;
+
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.number}</p>
+        <button onClick={() => store.dispatch({ type: "ADD" })}>+</button>
+        <button onClick={() => store.dispatch({ type: "MINUS" })}>-</button>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              store.dispatch({ type: "ADD" });
+            }, 1000);
+          }}
+        >
+          1秒后加1
+        </button>
+      </div>
+    );
+  }
+}
--- react核心及周边/myredux/src/index.js
+++ react核心及周边/myredux/src/index.js
@@ -1,40 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { createStore } from "./redux";
-
-let counterValue = document.getElementById("counter-value");
-let addBtn = document.getElementById("add-btn");
-let minusBtn = document.getElementById("minus-btn");
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-let initState = { number: 0 };
-
-const reducer = (state = initState, action) => {
-  switch (action.type) {
-    case ADD:
-      return { number: state.number + 1 };
-    case MINUS:
-      return { number: state.number - 1 };
-    default:
-      return state;
-  }
-};
-
-let store = createStore(reducer);
-function render() {
-  counterValue.innerText = store.getState().number + "";
-}
-
-store.subscribe(render);
-render();
-addBtn.addEventListener("click", () => {
-  store.dispatch({ type: ADD });
-});
-minusBtn.addEventListener("click", () => {
-  store.dispatch({ type: MINUS });
-});
+import Counter1 from "./components/Counter1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Counter1 />);
