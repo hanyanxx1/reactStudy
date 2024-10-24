9d3adadafd9205b6488888940c4187b499fa2ad8
hanyanxx1 <hanyanxx1@163.com>
2024-10-24 10:09:52 +0800

    saga 4. 计数器

--- react核心及周边/saga/src/index.js
+++ react核心及周边/saga/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Counter from "./Counter";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
---
+++ react核心及周边/saga/src/store/rootSaga.js
@@ -0,0 +1,22 @@
+import { put, take } from "redux-saga/effects";
+import * as types from "./action-types";
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function* workerSaga() {
+  yield delay(1000);
+  yield put({ type: types.ADD });
+}
+
+function* watcherSaga() {
+  yield take(types.ASYNC_ADD);
+  yield workerSaga();
+}
+
+export default function* rootSaga() {
+  yield watcherSaga();
+}
