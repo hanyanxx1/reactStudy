bafd160855bc5f9bb1b33625e227d62c57a10ac7
hanyanxx1 <hanyanxx1@163.com>
2024-10-27 17:48:27 +0800

    redux_tookit  RTK Query Quick Start RTK

--- react核心及周边/my_redux_toolkit/src/App.js
+++ react核心及周边/my_redux_toolkit/src/App.js
@@ -1,7 +1,21 @@
-import { Counter } from "./features/counter/Counter";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 const App = () => {
-  return <Counter />;
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  return (
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
 };
 
 export default App;
--- react核心及周边/my_redux_toolkit/src/index.js
+++ react核心及周边/my_redux_toolkit/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./app/store";
+import { store } from "./store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
