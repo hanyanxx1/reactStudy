c268de9762cdb3ca24255db9759061d30e6670cd
hanyanxx1 <hanyanxx1@163.com>
2024-10-27 17:30:51 +0800

    redux_tookit  基本使用

---
+++ react核心及周边/my_redux_toolkit/src/App.js
@@ -0,0 +1,7 @@
+import { Counter } from "./features/counter/Counter";
+
+const App = () => {
+  return <Counter />;
+};
+
+export default App;
--- react核心及周边/my_redux_toolkit/src/index.js
+++ react核心及周边/my_redux_toolkit/src/index.js
@@ -1,39 +1,10 @@
-import { createStore } from "redux";
-const ADD = "ADD";
-const MINUS = "MINUS";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-function add() {
-  return { type: ADD };
-}
-
-function minus() {
-  return { type: MINUS };
-}
-function counter(state = { number: 0 }, action) {
-  switch (action.type) {
-    case ADD:
-      return { number: state.number + 1 };
-    case MINUS:
-      return { number: state.number - 1 };
-    default:
-      return state;
-  }
-}
-
-var store = createStore(counter);
-var valueEl = document.getElementById("value");
-
-function render() {
-  valueEl.innerHTML = store.getState().number;
-}
-
-render();
-store.subscribe(render);
-
-document.getElementById("add").addEventListener("click", function () {
-  store.dispatch(add());
-});
-
-document.getElementById("minus").addEventListener("click", function () {
-  store.dispatch(minus());
-});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
